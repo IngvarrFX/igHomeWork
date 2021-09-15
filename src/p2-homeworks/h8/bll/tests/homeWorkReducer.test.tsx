9e39856a5e4f316765e3b460ea2680ae06b34174
IngvarrFX <64922169+IngvarrFX@users.noreply.github.com>
2021-09-15 15:58:01 +0300

    Added additional reducer checks in the test

--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -4,14 +4,7 @@ import {InitialPeopleType} from "../../HW8";
 
 
 
-let initialState: InitialPeopleType = [
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
-]
+let initialState: InitialPeopleType
 
 beforeEach(() => {
     initialState= [
@@ -29,6 +22,10 @@ test('sort name up', () => {
 
     console.log(newState)
     expect(newState[0].name).toBe('Александр')
+
+    const result = [1,3,4,5, 2,0].filter((el,i) => el === newState[i]._id)
+    expect(result.length).toBe(newState.length)
+
 })
 test('sort name down', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
